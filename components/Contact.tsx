ef1396a1f004bfea1d23b58c6ceee1d975c9b1e7
DevLukab <dev.lukab@gmail.com>
2025-02-14 14:56:08 +0100

    responsive changes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 
 export const Contact = () => {
     return (
-        <section id='contact' className='overflow-x-clip py-32 text-white max-w-[1200px] mx-auto px-4'>
+        <section id='contact' className='overflow-x-clip py-32 text-white max-w-[1200px] mx-auto px-6 sm:px-4 space-y-8'>
             <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
@@ -36,7 +36,7 @@ export const Contact = () => {
                         <div className='space-y-2'>
                             <p className='text-lg text-gray-300'>Email</p>
                             <a href="mailto: dev.lukab@gmail.com"
-                                className='text-3xl lg:text-4xl font-semibold hover:text-purple-400 transition duration-300 flex items-center gap-2'>
+                                className='text-3xl lg:text-4xl font-semibold hover:text-purple-400 transition duration-300 flex items-center gap-2 break-all'>
                                 dev.lukab@gmail.com
                                 <span className='text-gray-500'>↗</span>
                             </a>
@@ -47,7 +47,7 @@ export const Contact = () => {
                     initial={{ opacity: 0, x: 20 }}
                     whileInView={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.6, delay: 0.6 }}
-                    className='w-full h-full min-h-[400] rounded-2xl overflow-hidden'>
+                    className='w-full aspect-[16/9] md:h-full min-h-[350px] rounded-2xl overflow-hidden'>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23770.29474382656!2d3.1212996547969047!3d41.86517461961626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12bbaad8439676e1%3A0x400fae021a46c00!2sPalam%C3%B3s%2C%20Girona!5e0!3m2!1ses!2ses!4v1739454891677!5m2!1ses!2ses"
                         width="100%"
