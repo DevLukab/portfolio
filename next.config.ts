531d11b91f2036a8fdfb2fad4d346eae40213309
DevLukab <dev.lukab@gmail.com>
2025-02-17 18:15:41 +0100

    gh-pages change

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
     unoptimized: true,
   },
+  ...(isProd && {
+    basePath: "/portfolio",
+    assetPrefix: "/portfolio",
+  }),
 };
 
 export default nextConfig;
