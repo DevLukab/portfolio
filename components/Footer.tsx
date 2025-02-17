f94a0194c475b65053085547b1986892856e66b7
DevLukab <dev.lukab@gmail.com>
2025-02-17 16:33:04 +0100

    footer text change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaArrowUp } from "react-icons/fa";
 export const Footer = () => {
     return (
         <div className="relative mt-12 text-white/70 py-8 px-4 max-w-[1200px] container mx-auto border-t border-gray-700 pt-4 flex justify-between items-center">
-            <h1 className="text-sm md:text-base">Copyright &copy; 2025 by Luka | All Rights Reserved.</h1>
+            <h1 className="text-sm md:text-base">Copyright &copy; 2025 by Luka</h1>
 
             <a href="#home" className="hover:text-purple-400 transition-colors duration-300">
                 <FaArrowUp size={24} />
